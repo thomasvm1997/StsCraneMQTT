bfb47b4ccebd32a487a3e93b07e88f316b3cc6b2
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-11-26 16:19:18 +0100

    Add folder and vs project

---
+++ Csharp/Wpe.SharkCrane/Wpe.SharkCrane.Cosnole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Wpe.SharkCrane.Cosnole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
