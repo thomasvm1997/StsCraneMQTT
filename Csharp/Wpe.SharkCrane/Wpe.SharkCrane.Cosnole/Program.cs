4e76c9d28e904e0eaceeca9d0ddccb111cd3919c
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-11-28 10:21:53 +0100

    Add name fix

--- Csharp/Wpe.SharkCrane/Wpe.SharkCrane.Cosnole/Program.cs
+++ Csharp/Wpe.SharkCrane/Wpe.SharkCrane.Cosnole/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace Wpe.SharkCrane.Cosnole
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
