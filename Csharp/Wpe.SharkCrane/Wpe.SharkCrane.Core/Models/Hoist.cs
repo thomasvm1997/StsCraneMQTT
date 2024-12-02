dba5c91d2b3c3df0b655e73144ff291550b35e4a
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-12-02 16:22:08 +0100

    Add object

---
+++ Csharp/Wpe.SharkCrane/Wpe.SharkCrane.Core/Models/Hoist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpe.SharkCrane.Core.Models
+{
+    public class Hoist
+    {
+        private double length;
+        public double Length 
+        
+        {
+            get {return length;}
+
+            set
+            {
+                if (value < 0)
+                {
+                    length = 0;
+                }
+                else
+                {
+                    length = value;
+                }
+            }
+        }
+    }
+}
