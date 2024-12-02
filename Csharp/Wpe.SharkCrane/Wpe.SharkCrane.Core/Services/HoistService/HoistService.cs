f687da5506865f10efb55edbf84c0c031926a176
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-12-02 15:15:07 +0100

    Add services and new console client

---
+++ Csharp/Wpe.SharkCrane/Wpe.SharkCrane.Core/Services/HoistService/HoistService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Wpe.SharkCrane.Core.Models.CustomEventArgs;
+using Wpe.SharkCrane.Core.Models;
+using Wpe.SharkCrane.Core.Services.HiveService.Interfaces;
+using Wpe.SharkCrane.Core.Services.HoistService.Interfaces;
+
+namespace Wpe.SharkCrane.Core.Services.HoistService
+{
+    public class HoistService : IHoistService
+    {
+        private readonly IHiveMQService _hiveMQService;
+        private Spreader mainSpreader;
+        public HoistService(IHiveMQService hiveMQService)
+        {
+            _hiveMQService = hiveMQService;
+            _hiveMQService.MessageReceived += OnMessageReceived;
+            mainSpreader = new Spreader();
+        }
+
+        public async void OnMessageReceived(object sender, CustomMessageReceivedEventArgs e)
+        {
+            var topic = "/spreader";
+            Console.WriteLine($"SpreaderService received message on topic /hub/spreader : {e.Payload}");
+
+
+            var spreader = JsonSerializer.Deserialize<Spreader>(e.Payload);
+
+            ChangeMainProperties(spreader);
+
+            var mainsString = JsonSerializer.Serialize(mainSpreader);
+
+            await _hiveMQService.PublishAsync(topic, mainsString);
+
+        }
+    }
+}
