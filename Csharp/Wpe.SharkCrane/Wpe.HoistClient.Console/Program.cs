1767e3f2d34685af0f7538ac5b2fe9bec661ffec
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-12-03 11:02:32 +0100

    Fix dependency injections

--- Csharp/Wpe.SharkCrane/Wpe.HoistClient.Console/Program.cs
+++ Csharp/Wpe.SharkCrane/Wpe.HoistClient.Console/Program.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Wpe.SharkCrane.Core.Services.HiveService;
+using Wpe.SharkCrane.Core.Services.HiveService.Interfaces;
+using Wpe.SharkCrane.Core.Services.HoistService;
+using Wpe.SharkCrane.Core.Services.HoistService.Interfaces;
+
 
 #region injections
 var serviceProvider = new ServiceCollection()
             .AddSingleton<IHiveMQService, HiveMQService>()
-            .AddSingleton<ISpreaderService, SpreaderService>().BuildServiceProvider();
+            .AddSingleton<IHoistService, HoistService>().BuildServiceProvider();
 
 
 var hiveClient = serviceProvider.GetRequiredService<IHiveMQService>();
-var spreaderService = serviceProvider.GetRequiredService<ISpreaderService>();
+var hoistService = serviceProvider.GetRequiredService<IHoistService>();
 await hiveClient.ConnectAsync();
 #endregion
 
-Console.WriteLine("blabla");
