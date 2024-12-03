7bc6ed36e741d1b396b0369a32714a9f578878e0
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-12-03 10:55:45 +0100

    add injections to hoist console

--- Csharp/Wpe.SharkCrane/Wpe.HoistClient.Console/Program.cs
+++ Csharp/Wpe.SharkCrane/Wpe.HoistClient.Console/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+
+#region injections
+var serviceProvider = new ServiceCollection()
+            .AddSingleton<IHiveMQService, HiveMQService>()
+            .AddSingleton<ISpreaderService, SpreaderService>().BuildServiceProvider();
+
+
+var hiveClient = serviceProvider.GetRequiredService<IHiveMQService>();
+var spreaderService = serviceProvider.GetRequiredService<ISpreaderService>();
+await hiveClient.ConnectAsync();
+#endregion
+
+Console.WriteLine("blabla");
