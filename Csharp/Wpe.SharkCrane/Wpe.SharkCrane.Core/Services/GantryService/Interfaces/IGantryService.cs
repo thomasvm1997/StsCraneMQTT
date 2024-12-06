c180afa9f74f2babd337974fa3e6fc1045b41295
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-12-06 16:21:37 +0100

    Make working gantry

---
+++ Csharp/Wpe.SharkCrane/Wpe.SharkCrane.Core/Services/GantryService/Interfaces/IGantryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpe.SharkCrane.Core.Models;
+using Wpe.SharkCrane.Core.Services.BaseService.Interfaces;
+
+namespace Wpe.SharkCrane.Core.Services.GantryService.Interfaces
+{
+    public interface IGantryService : IBaseCraneService<Gantry>
+    {
+
+    }
+}
