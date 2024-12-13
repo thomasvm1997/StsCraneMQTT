ae21cc41ea53128a52266770111e716b0401982f
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-12-13 14:12:03 +0100

    Add interfaces and basic service class to spreader

---
+++ Csharp/Wpe.SharkCrane/Wpe.SharkCrane.Core/Services/TrolleyService/Interfaces/ITrolleyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpe.SharkCrane.Core.Models;
+using Wpe.SharkCrane.Core.Services.BaseService.Interfaces;
+
+namespace Wpe.SharkCrane.Core.Services.TrolleyService.Interfaces
+{
+    public interface ITrolleyService : IBaseCraneService<Trolley>
+    {
+
+    }
+}
